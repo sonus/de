638e5400faeece164b4b2a3ab762deac9d9ff794
Sonus Sundar <sonusmsundar@gmail.com>
2023-02-08 17:26:48 +0530

    :YTEst:

--- js/script.js
+++ js/script.js
@@ -6,69 +6,12 @@ $(window).on('load', function () {
 (function ($) {
   'use strict';
 
-  
-  // product Slider
-  $('.product-image-slider').slick({
-    autoplay: false,
-    infinite: true,
-    arrows: false,
-    dots: true,
-    customPaging: function (slider, i) {
-      var image = $(slider.$slides[i]).data('image');
-      return '<img class="img-fluid" src="' + image + '" alt="product-image">';
-    }
-  });
 
-  // Product slider
-  $('.product-slider').slick({
-    infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: true,
-    dots: false,
-    arrows: false,
-    responsive: [{
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
-
-
-  gridGallery({
-    selector: ".dark",
-    darkMode: true
-  });
+if($("#horizontal").length){
   gridGallery({
     selector: "#horizontal",
     layout: "horizontal"
   });
-  gridGallery({
-    selector: "#square",
-    layout: "square"
-  });
-  gridGallery({
-    selector: "#gap",
-    gaplength: 10
-  });
-  gridGallery({
-    selector: "#heightWidth",
-    rowHeight: 180,
-    columnWidth: 280
-  });
+}
 
 })(jQuery);
